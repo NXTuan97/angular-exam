c3c8526f33cbae8ab12e795828a4afbe4c9c7bd8
NXTuan97 <nguyenxuantuannxt@gmail.com>
2022-07-30 10:10:13 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IExam} from "./interface/exam.interface";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'examAngular';
+
+  // examArray: IExam[]=[
+  //   {name:'Gemma Roberson', status:'Apology for late response email', help:'Hello Colette Wooten'},
+  //   {name:'Anna Garza', status:'Application for job title', help:'Hello Kerry Best'},
+  //   {name:'Alfonso Burnett', status:'Anything I can help with', help:'Hello Otto Ashley'},
+  //   {name:'Rogan Espinoza', status:'Assistant Marketing Department', help:'Hello Kerry Best'},
+  //   {name:'Sierra Kerr', status:'Application of transfer', help:'Hi Halle lindsey'},
+  // ];
 }
---
+++ src/app/exam/exam.component.ts
@@ -0,0 +1,19 @@
+import {Component,Input} from "@angular/core";
+import {IExam} from "../interface/exam.interface";
+
+@Component({
+  selector:'app-exam',
+  templateUrl: './exam.component.html'
+})
+export class ExamComponent{
+ @Input()
+  file!: IExam;
+
+  examArray: IExam[]=[
+    {name:'Gemma Roberson', status:'Apology for late response email', help:'Hello Colette Wooten'},
+    {name:'Anna Garza', status:'Application for job title', help:'Hello Kerry Best'},
+    {name:'Alfonso Burnett', status:'Anything I can help with', help:'Hello Otto Ashley'},
+    {name:'Rogan Espinoza', status:'Assistant Marketing Department', help:'Hello Kerry Best'},
+    {name:'Sierra Kerr', status:'Application of transfer', help:'Hi Halle lindsey'},
+  ];
+}
